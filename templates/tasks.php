dbcb7d8a7a32e5d803a1888ef7a746eb29f2ded3
Josh999999 <jjmorgan2003@gmail.com>
2023-05-03 17:55:43 +0100

    Test commit to see what happens to the task file

--- templates/tasks.php
+++ templates/tasks.php
@@ -1,1 +1,10 @@
-<p>You currently have 0 tasks</p>
+<div class="container">
+    <div class="container" id="taskListContainer">
+        <ul class="taskList">
+            <?php 
+                //Php code to display all of the users tasks 
+            ?>
+            <li class="taskListItem" id="displayItem">DISPLAY TASK 1</li>
+        </ul>
+    </div>
+</div>
